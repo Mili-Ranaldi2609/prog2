5f9e1b28499676fcb007b6d06903f234f1a9d41d
Mili Ranaldi <mranaldi@gmail.com>
2024-04-04 16:07:01 -0300

    ---

--- clase4_4/src/Ejercicio1/Cuadrado.java
+++ clase4_4/src/Ejercicio1/Cuadrado.java
@@ -1,4 +1,32 @@
 package Ejercicio1;
 
-public class Cuadrado {
+public class Cuadrado implements Figura,Rotable,Dibujable {
+    private double lado;
+
+    public Cuadrado(double lado) {
+        this.lado = lado;
+    }
+
+    public double getLado() {
+        return lado;
+    }
+
+    public void setLado(double lado) {
+        this.lado = lado;
+    }
+
+    @Override
+    public void dibujar() {
+        System.out.println("Se dibujo el cuadrado ■ ");
+    }
+
+    @Override
+    public double area() {
+        return lado*lado;
+    }
+
+    @Override
+    public void rotar() {
+        System.out.println("Se roto el cuadrado ■■");
+    }
 }
