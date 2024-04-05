dabd573d65382063f97cb7fa9a0ee2de9a5ae0a1
Mili Ranaldi <mranaldi@gmail.com>
2024-04-04 21:02:33 -0300

    ---

--- clase4_4/src/Ejercicio2/Picaro.java
+++ clase4_4/src/Ejercicio2/Picaro.java
@@ -1,2 +1,51 @@
-package Ejercicio2;public class Picaro {
+package Ejercicio2;
+
+public class Picaro extends PersonajeBase{
+    private int daño;
+    private int defensa;
+
+    public Picaro(String nombre, int nivel,int rpg,int daño,int defensa) {
+        super(nombre, nivel,rpg);
+        this.daño=daño;
+        this.defensa=defensa;
+    }
+
+    public int getDaño() {
+        return daño;
+    }
+
+    public void setDaño(int daño) {
+        this.daño = daño;
+    }
+
+    public int getDefensa() {
+        return defensa;
+    }
+
+    public void setDefensa(int defensa) {
+        this.defensa = defensa;
+    }
+
+    @Override
+    public int defender(int daño) {
+        int dañoReduccido = daño / 2;
+        // Aplicar defensa adicional
+        dañoReduccido -= this.defensa;
+
+        if (dañoReduccido > 0) {
+            int r=this.getRpg();
+            r -= dañoReduccido;
+            this.setRpg(r);
+            System.out.println(getNombre() + " ha recibido " + dañoReduccido + " puntos de daño.");
+        } else {
+            System.out.println(getNombre() + " ha bloqueado todo el daño.");
+        }
+        return dañoReduccido;
+    }
+
+    @Override
+    public int atacar() {
+        int ataque=(daño*this.getNivel())/2;
+        return ataque;
+    }
 }
