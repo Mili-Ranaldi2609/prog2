e4ff87ffb94ca37238bb2de3e224fc1039799345
Mili Ranaldi <mranaldi@gmail.com>
2024-04-04 21:04:10 -0300

    ---

--- clase4_4/src/Ejercicio2/PersonajeBase.java
+++ clase4_4/src/Ejercicio2/PersonajeBase.java
@@ -1,2 +1,40 @@
-package Ejercicio2;public class PersonajeBase {
+package Ejercicio2;
+
+public abstract class PersonajeBase implements Personaje{
+    private String nombre;
+    private int nivel;
+    private int rpg;
+
+    public PersonajeBase(String nombre,int nivel,int rpg) {
+        this.nivel = nivel;
+        this.nombre= nombre;
+        this.rpg=rpg;
+    }
+
+    public int getRpg() {
+        return rpg;
+    }
+
+    public void setRpg(int rpg) {
+        this.rpg = rpg;
+    }
+
+    public int getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(int nivel) {
+        this.nivel = nivel;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public boolean estaVivo() {
+        return rpg > 0;
+    }
 }
